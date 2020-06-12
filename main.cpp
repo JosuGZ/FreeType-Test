8e9449101355d49cec5b5171e71afb75943c1f6c
Josu Goñi <josu_z@hotmail.com>
2020-06-12 17:31:28 +0200

    Test stb for writing images

--- main.cpp
+++ main.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
+#include <cassert>
 
 #include <ft2build.h>
 #include FT_FREETYPE_H
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include <stb_image_write.h>
 
 using std::cout;
 using std::endl;
 
 static const char *fontFile = "/usr/share/fonts/truetype/liberation/LiberationSerif-Regular.ttf";
+
+static void testPng();
 
 int main() {
   FT_Library freeType;
@@ -43,5 +49,22 @@ int main() {
   cout << "num_fixed_sizes: " << fontFace->num_fixed_sizes << endl;
   cout << "available_sizes: " << fontFace->available_sizes << endl;
 
+  testPng();
+
   return 0;
+}
+
+static const char *outputFile = "Test.png";
+
+static void testPng() {
+  uint8_t data[1000 * 1000 * 3] {};
+  for (int i=0; i<1000; ++i) {
+    for (int j=0; j<1000; ++j) {
+      data[i * 3 * 1000 + j * 3 + 0] = i;
+      data[i * 3 * 1000 + j * 3 + 1] = j;
+      data[i * 3 * 1000 + j * 3 + 2] = 0xFF;
+    }
+  }
+  int success = stbi_write_png(outputFile, 1000, 1000, 3, data, 1000 * 3);
+  assert(success);
 }
